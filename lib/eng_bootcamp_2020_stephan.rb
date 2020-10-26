0d471be70f219780caf6ea7058c1b8ad4beea44c
Stephan Hagemann <stephan.hagemann@gusto.com>
2020-10-26 16:28:24 -0600

    Method that outputs on startup

--- lib/eng_bootcamp_2020_stephan.rb
+++ lib/eng_bootcamp_2020_stephan.rb
@@ -2,5 +2,11 @@ require "eng_bootcamp_2020_stephan/version"
 
 module EngBootcamp2020Stephan
   class Error < StandardError; end
+
+  def self.hello_gusto
+    "hello gusto"
+  end
   # Your code goes here...
 end
+
+puts EngBootcamp2020Stephan.hello_gusto
--- spec/eng_bootcamp_2020_stephan_spec.rb
+++ spec/eng_bootcamp_2020_stephan_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe EngBootcamp2020Stephan do
   it "does something useful" do
     expect(true).to eq(true)
   end
+
+  it 'can output hello gusto' do
+    expect(EngBootcamp2020Stephan.hello_gusto).to eq('hello gusto')
+  end
+
+  it 'outputs hello gusto when loaded' do
+    pending
+    expect(false).to eq(true)
+  end
 end
