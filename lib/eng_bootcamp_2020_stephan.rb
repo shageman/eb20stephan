e59d947da84606c37a92640af9b5f1703ee0bf05
Stephan Hagemann <stephan.hagemann@gusto.com>
2020-10-26 16:43:36 -0600

    Different impl + tests

--- lib/eng_bootcamp_2020_stephan.rb
+++ lib/eng_bootcamp_2020_stephan.rb
@@ -3,10 +3,32 @@ require "eng_bootcamp_2020_stephan/version"
 module EngBootcamp2020Stephan
   class Error < StandardError; end
 
+  def self.env=(value)
+    @configured_env = value
+  end
+
   def self.hello_gusto
     "hello gusto"
   end
+
+  def self.hello_gusto_with_env_param(env)
+    "hello gusto - ENV #{env}"
+  end
+
+  def self.hello_gusto_with_rails_env
+    env_string = defined?(Rails) ? " - ENV #{Rails.env}" : ""
+    "hello gusto#{env_string}"
+  end
+
+  def self.hello_gusto_with_configured_env
+    env_string = @configured_env ? " - ENV #{@configured_env}" : ""
+    "hello gusto#{env_string}"
+  end
+
   # Your code goes here...
 end
 
 puts EngBootcamp2020Stephan.hello_gusto
+puts EngBootcamp2020Stephan.hello_gusto_with_env_param('some env')
+puts EngBootcamp2020Stephan.hello_gusto_with_rails_env
+puts EngBootcamp2020Stephan.hello_gusto_with_configured_env
--- spec/eng_bootcamp_2020_stephan_spec.rb
+++ spec/eng_bootcamp_2020_stephan_spec.rb
@@ -7,12 +7,48 @@ RSpec.describe EngBootcamp2020Stephan do
     expect(true).to eq(true)
   end
 
-  it 'can output hello gusto' do
-    expect(EngBootcamp2020Stephan.hello_gusto).to eq('hello gusto')
+  describe '.hello_gusto' do
+    it 'can output hello gusto' do
+      expect(EngBootcamp2020Stephan.hello_gusto).to eq('hello gusto')
+    end
+
+    it 'outputs hello gusto when loaded' do
+      pending
+      expect(false).to eq(true)
+    end
   end
 
-  it 'outputs hello gusto when loaded' do
-    pending
-    expect(false).to eq(true)
+  describe '.hello_gusto_with_env_param' do
+    it 'can output hello gusto' do
+      expect(EngBootcamp2020Stephan.hello_gusto_with_env_param('MY_ENV')).to eq('hello gusto - ENV MY_ENV')
+    end
+  end
+
+  describe '.hello_gusto_with_rails_env' do
+    it 'can output hello gusto when there is no Rails env' do
+      expect(EngBootcamp2020Stephan.hello_gusto_with_rails_env).to eq('hello gusto')
+    end
+
+    it 'can output hello gusto with Rails env' do
+      Rails = double()
+      allow(Rails).to receive(:env).and_return :TEST
+
+      expect(EngBootcamp2020Stephan.hello_gusto_with_rails_env).to eq('hello gusto - ENV TEST')
+    end
+  end
+
+  describe '.hello_gusto_with_configured_env' do
+    before do
+      EngBootcamp2020Stephan.env = nil
+    end
+
+    it 'can output hello gusto when no env is configured' do
+      expect(EngBootcamp2020Stephan.hello_gusto_with_configured_env).to eq('hello gusto')
+    end
+
+    it 'can output hello gusto with configured env' do
+      EngBootcamp2020Stephan.env = 'CONFIGURED TEST'
+      expect(EngBootcamp2020Stephan.hello_gusto_with_configured_env).to eq('hello gusto - ENV CONFIGURED TEST')
+    end
   end
 end
