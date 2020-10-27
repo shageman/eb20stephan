9360b66d83950e3e3010b279121a85593b400c67
Peter Compernolle <peter.compernolle@gusto.com>
2020-10-27 08:57:19 -0500

    moves rspec subject into explicit block in each describe block

--- spec/eng_bootcamp_2020_stephan_spec.rb
+++ spec/eng_bootcamp_2020_stephan_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe EngBootcamp2020Stephan do
   end
 
   describe '.hello_gusto' do
+    subject { described_class.hello_gusto }
+
     it 'can output hello gusto' do
-      expect(EngBootcamp2020Stephan.hello_gusto).to eq('hello gusto')
+      expect(subject).to eq('hello gusto')
     end
 
     it 'outputs hello gusto when loaded' do
@@ -19,36 +21,47 @@ RSpec.describe EngBootcamp2020Stephan do
   end
 
   describe '.hello_gusto_with_env_param' do
+    let(:env_param) { 'MY_ENV' }
+    subject { described_class.hello_gusto_with_env_param(env_param) }
+
     it 'can output hello gusto' do
-      expect(EngBootcamp2020Stephan.hello_gusto_with_env_param('MY_ENV')).to eq('hello gusto - ENV MY_ENV')
+      expect(subject).to eq('hello gusto - ENV MY_ENV')
     end
   end
 
   describe '.hello_gusto_with_rails_env' do
-    it 'can output hello gusto when there is no Rails env' do
-      expect(EngBootcamp2020Stephan.hello_gusto_with_rails_env).to eq('hello gusto')
+    subject { described_class.hello_gusto_with_rails_env }
+
+    context 'when Rails is not defined' do
+      it 'can output hello gusto when there is no Rails env' do
+        expect(subject).to eq('hello gusto')
+      end
     end
 
-    it 'can output hello gusto with Rails env' do
-      Rails = double()
-      allow(Rails).to receive(:env).and_return :TEST
+    context 'when Rails is defined' do
+      let(:rails_env) { :TEST }
 
-      expect(EngBootcamp2020Stephan.hello_gusto_with_rails_env).to eq('hello gusto - ENV TEST')
+      before do
+        Rails = double()
+        allow(Rails).to receive(:env).and_return rails_env
+      end
+
+      it 'can output hello gusto with Rails env' do
+        expect(subject).to eq('hello gusto - ENV TEST')
+      end
     end
   end
 
   describe '.hello_gusto_with_configured_env' do
-    before do
-      EngBootcamp2020Stephan.env = nil
-    end
+    subject { described_class.hello_gusto_with_configured_env }
 
     it 'can output hello gusto when no env is configured' do
-      expect(EngBootcamp2020Stephan.hello_gusto_with_configured_env).to eq('hello gusto')
+      expect(subject).to eq('hello gusto')
     end
 
     it 'can output hello gusto with configured env' do
       EngBootcamp2020Stephan.env = 'CONFIGURED TEST'
-      expect(EngBootcamp2020Stephan.hello_gusto_with_configured_env).to eq('hello gusto - ENV CONFIGURED TEST')
+      expect(subject).to eq('hello gusto - ENV CONFIGURED TEST')
     end
   end
 end
