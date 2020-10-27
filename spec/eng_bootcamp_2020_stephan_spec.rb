882156a11b6bc5ffc8b050daf9625b659fe7b588
Peter Compernolle <peter.compernolle@gusto.com>
2020-10-27 08:58:06 -0500

    moves assignment ofEngBootcamp2020Stephan env into around block to ensure singleton value does not leak between tests

--- spec/eng_bootcamp_2020_stephan_spec.rb
+++ spec/eng_bootcamp_2020_stephan_spec.rb
@@ -55,13 +55,26 @@ RSpec.describe EngBootcamp2020Stephan do
   describe '.hello_gusto_with_configured_env' do
     subject { described_class.hello_gusto_with_configured_env }
 
-    it 'can output hello gusto when no env is configured' do
-      expect(subject).to eq('hello gusto')
+    let(:configured_env) { nil }
+
+    around(:each) do |example|
+      previous_env = EngBootcamp2020Stephan.instance_variable_get(:@env)
+      EngBootcamp2020Stephan.env = configured_env
+      EngBootcamp2020Stephan.env = previous_env
     end
 
-    it 'can output hello gusto with configured env' do
-      EngBootcamp2020Stephan.env = 'CONFIGURED TEST'
-      expect(subject).to eq('hello gusto - ENV CONFIGURED TEST')
+    context 'when no env is configured' do
+      it 'can output hello gusto when no env is configured' do
+        expect(subject).to eq('hello gusto')
+      end
+    end
+
+    context 'when an env is configured' do
+      let(:configured_env) { 'CONFIGURED TEST' }
+
+      it 'can output hello gusto with configured env' do
+        expect(subject).to eq('hello gusto - ENV CONFIGURED TEST')
+      end
     end
   end
 end
